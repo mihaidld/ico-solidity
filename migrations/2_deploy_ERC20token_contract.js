7d7edfe2fd1da1b2c1e2f0bb535a8dd16789ea53
mihaidld <mihaidoldur@gmail.com>
2020-11-06 18:07:06 +0100

    typo

--- migrations/2_deploy_ERC20token_contract.js
+++ migrations/2_deploy_ERC20token_contract.js
@@ -1,12 +1,8 @@
-// FirstErc20 is an artifact of the NewToken contract
+// NewToken is an artifact of the NewToken contract
 const NewToken = artifacts.require('NewToken');
 const { BN } = require('@openzeppelin/test-helpers');
 
 module.exports = async (deployer) => {
-  // All the abstractions/instances below are not needed
-  // They are only useful if we need to interact with it
-  // for further deployments.
-
   await deployer.deploy(
     NewToken,
     'NewToken',
